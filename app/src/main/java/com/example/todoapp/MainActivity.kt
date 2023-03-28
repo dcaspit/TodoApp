2148d447eef2d97c5450db457b8d058049c03985
Daniel Caspit <dcaspit@gmail.com>
2023-03-28 17:59:48 +0300

    Init App

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
