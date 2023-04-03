a4805a5c22a2e19e02c36b81ae5dfa01dcf78732
dcaspit <101496440+dcaspit365@users.noreply.github.com>
2023-04-03 21:45:28 +0300

    Designed ListFragment and advanced with coding

--- app/src/main/java/com/example/todoapp/fragments/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/ListFragment.kt
@@ -5,24 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
+
+    private val navController = findNavController()
+    private lateinit var view: View
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_list, container, false)
-
-        val floatingActionButton = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
-
-        floatingActionButton.setOnClickListener {
-            findNavController().navigate(R.id.action_listFragment_to_addFragment)
-        }
-
+        view = inflater.inflate(R.layout.fragment_list, container, false)
+        setFloatingClickListener()
+        setListLayoutClickListener()
         return view
     }
+
+    private fun findFloatingButton() = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+    private fun findListLayout() = view.findViewById<ConstraintLayout>(R.id.listLayout)
+    private fun setFloatingClickListener(){
+        findFloatingButton().setOnClickListener {
+            navController.navigate(R.id.action_listFragment_to_addFragment)
+        }
+    }
+    private fun setListLayoutClickListener(){
+        findListLayout().setOnClickListener {
+            navController.navigate(R.id.action_listFragment_to_updateFragment)
+        }
+    }
+
 }
